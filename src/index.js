1a3d5626dec8175fb0650ce6d161187818000310
Randy Harkediansa <randy.harkediansa@gmail.com>
2019-05-17 17:43:23 +0800

    Minor code cleanup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const axios = require('axios');
 
 const getAllDependencies = async (packageName) => {
-
-    
     try {
         const allDependencies = [];
 
         await axios.get(`http://registry.npmjs.org/${packageName}/latest`)
-        .then(function (response) {          
-            let arrDependencies = Object.keys(response.data.dependencies);
+          .then(function (response) {          
+              let arrDependencies = Object.keys(response.data.dependencies);
 
-            for (let x = 0; x < arrDependencies.length; x++) {
-                allDependencies.push(arrDependencies[x]);
-            }
-        })
-        .catch(function (error) {
-            throw new Error(error);
-        });
+              for (let x = 0; x < arrDependencies.length; x++) {
+                  allDependencies.push(arrDependencies[x]);
+              }
+          })
+          .catch(function (error) {
+              throw new Error(error);
+          });
 
         // for (let y = 0; y < allDependencies.length; y++) {
         //     const pkg = getAllDependencies(allDependencies[y]);
